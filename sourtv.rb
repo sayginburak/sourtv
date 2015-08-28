6926ab062dfc87e776ac36bad2c7f5ae2c5f0e45
Saygın Burak <sayginburak17@gmail.com>
2015-08-28 17:58:09 +0300

    Loop through each season page and each episode page and fetch episode images and airdates

--- sourtv.rb
+++ sourtv.rb
@@ -10,6 +10,17 @@ seasons = seasons_array.map do |s|
 	}
 end
 puts seasons
-seasons.each do |s| 
+data = {}
+seasons.each_with_index do |s, j| 
+	season_no = (j+1).to_s.to_sym
 	season_html = Nokogiri::HTML(open(base_url+s[:link]))
+	data[season_no] = {}
+	season_html.css('.eplist').css('.list_item').each_with_index do |item,i|
+		episode_no = (i+1).to_s.to_sym
+		data[season_no][episode_no] = {}
+		data[season_no][episode_no][:image] = item.css('.image').css('a').first['href']
+		data[season_no][episode_no][:air_date] = item.css('.info').css('.airdate').first.text.strip.gsub(/\s+/, " ")
+	end
 end
+
+puts data
